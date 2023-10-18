6225b65952da1ebc651157dea45e3afd0603e545
panda <p4nd4@posteo.de>
2023-10-18 19:06:08 +0200

    update options arrays

--- main.js
+++ main.js
@@ -10,10 +10,10 @@ class Question{
     return 0;
   }
 };
-const firstQuestion = new Question("Was ist der größte Berg der Erde?",['Mount Everest', 'Kangchendzönga', 'Annapurna']);
+const firstQuestion = new Question("Was ist der größte Berg der Erde?",[['Mount Everest', 1], ['Kangchendzönga', 0], ['Annapurna', 0]]);
 
-const secondQuestion = new Question("Welches gehört zu den sieben Weltwundern?", ['Die Pyramiden von Gizeh','Eiffelturm', 'Brandenburger Tor']);
+const secondQuestion = new Question("Welches gehört zu den sieben Weltwundern?", [['Die Pyramiden von Gizeh', 1],['Eiffelturm', 0], ['Brandenburger Tor', 0]]);
 
-const thirdQuestion = new Question("Seit wann gibt es Bier?", ['Seit ca. 7000 v.u.Z', 'seit ca 2000 Jahren', 'seit 1516'])
+const thirdQuestion = new Question("Seit wann gibt es Bier?", [['Seit ca. 7000 v.u.Z', 1], ['seit ca 2000 Jahren', 0], ['seit 1516', 0]])
 
 console.log(secondQuestion.questionAndOptions())
