117d3559ff84c3c6224c8df964044d78d618ff2a
panda <p4nd4@posteo.de>
2023-10-18 22:35:02 +0200

    get DOM-elements, display question and answer

--- main.js
+++ main.js
@@ -4,16 +4,36 @@ class Question{
     this.options = options;
   }
   questionAndOptions(){
-    return `<div id=question>${this.text}</div> ${this.options.map(option => `<div>${option[0]}</div>`)}`
+    return `<div id=question>${this.text}</div> ${this.options.map(option => `<div class=option>${option[0]}</div>`)}`
   }
   checkChoice(){ 
     return 0;
   }
 };
+
+// --------- questions --------------
 const firstQuestion = new Question("Was ist der größte Berg der Erde?",[['Mount Everest', 1], ['Kangchendzönga', 0], ['Annapurna', 0]]);
 
 const secondQuestion = new Question("Welches gehört zu den sieben Weltwundern?", [['Die Pyramiden von Gizeh', 1],['Eiffelturm', 0], ['Brandenburger Tor', 0]]);
 
 const thirdQuestion = new Question("Seit wann gibt es Bier?", [['Seit ca. 7000 v.u.Z', 1], ['seit ca 2000 Jahren', 0], ['seit 1516', 0]])
 
-console.log(secondQuestion.questionAndOptions())
+//console.log(secondQuestion.questionAndOptions());
+
+// ----------- html-stuff ----------
+let currentQuestion = firstQuestion // need code to toggle
+const optionDivs = document.getElementsByClassName('option');
+const question = document.getElementById("question");
+console.log(optionDivs);
+const optionDivs2 = document.querySelectorAll('.option');
+console.log(optionDivs2);
+
+for (let i = 0; i < optionDivs.length; i++) {
+  optionDivs.addEventListener("click", test);
+}
+
+question.innerHTML = firstQuestion.questionAndOptions();
+
+function test() {
+  console.log('yay!')
+};
