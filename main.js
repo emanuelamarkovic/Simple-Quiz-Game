1be6f426ceb71817a35d246cfee08a5cece3d74e
panda <p4nd4@posteo.de>
2023-10-18 18:03:04 +0200

    fix spacing

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
 class Question{
-    constructor(text, options){
-        this.text = text;
-        this.options = options;
-    }
-    questionAndOptions(){
-        return `${}`
-    };
-}
-
-chekChoice(){
-    return
+  constructor(text, options){
+    this.text = text;
+    this.options = options;
+  }
+  questionAndOptions(){
+    return 0;
+  }
+  checkChoice(){ 
+    return 0;
+  }
 };
 
