d42f08f0b7c503c69f148bad7ab11e08c54cfb4b
panda <p4nd4@posteo.de>
2023-10-18 18:44:25 +0200

    add questionsAndOptions method

--- main.js
+++ main.js
@@ -4,11 +4,16 @@ class Question{
     this.options = options;
   }
   questionAndOptions(){
-    return 0;
+    return `<div id=question>${this.text}</div> <div>${this.options}</div>` 
   }
   checkChoice(){ 
     return 0;
   }
 };
 const firstQuestion = new Question("Was ist der größte Berg der Erde?",['Mount Everest', 'Kangchendzönga', 'Annapurna']);
-const secondQuestion = new Question("Welches gehört zu den sieben Weltwunder?", ['Die Pyramiden von Gizeh','Eiffelturm', 'Brandenburger Tor']);
+
+const secondQuestion = new Question("Welches gehört zu den sieben Weltwundern?", ['Die Pyramiden von Gizeh','Eiffelturm', 'Brandenburger Tor']);
+
+const thirdQuestion = new Question("Seit wann gibt es Bier?", ['Seit ca. 7000 v.u.Z', 'seit ca 2000 Jahren', 'seit 1516'])
+
+console.log(secondQuestion.questionAndOptions())
