fe8dcf032f4af2d5d4ba516a5bd0222c8f769bca
panda <p4nd4@posteo.de>
2023-11-07 13:55:43 +0100

    move script from memory.html to memory.js

--- memory.js
+++ memory.js
@@ -3,7 +3,23 @@
 // function unflip
 // function checkForMatch
 
+const emojis = ["🐼", "🐼", "🐢", "🐢", "😍", "😍", "🐒", "🐒"];
+const resetButton = document.getElementById("reset-button");
+// create cards
+let shuffledEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
+for (let i = 0; i < emojis.length; i++) {
+  const cards = document.createElement("div");
+  cards.className = "memory-card";
+  cards.innerHTML = shuffledEmojis[i];
+  document.querySelector("#memory-board").appendChild(cards);
+}
 const cards = document.querySelectorAll('.memory-card');
+
+function resetGame() {
+  window.location.reload();
+}
+resetButton.addEventListener("click", resetGame);
+
 // are any cards already flipped?
 let hasCardFlipped = false;
 // for a pair of cards
