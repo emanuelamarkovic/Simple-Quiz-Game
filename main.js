dc0186ef58657b8d33480f925b58e6d092b98b4e
panda <p4nd4@posteo.de>
2023-11-09 18:15:14 +0100

    navbar: add burger icon

--- main.js
+++ main.js
@@ -2,24 +2,30 @@
 const header = document.querySelector('header');
 const dropdownButton = document.createElement('div');
 dropdownButton.classList = 'dropdown-menue';
-dropdownButton.innerHTML = `<h2 class='dropdown-menue'>X</h><div class="options"></div`;
-header.appendChild(dropdownButton);
-const options = document.getElementsByClassName('options');
-options[0].style.display = 'none';
-//console.log(options);
-const htmlFiles = ['memory.html', 'index.html'];
-htmlFiles.forEach(htmlFile => {
-  console.log('htmlfile ', htmlFile);
-  const option = document.createElement('option');
-  const linkNameArr = htmlFile.split('.');
-  option.innerHTML = `<a href=${htmlFile}>${linkNameArr[0]}</a>`;
-  options[0].appendChild(option);
-})
+// load burger menue icon
+fetch('burger-icon.svg')
+    .then(response => response.text())
+    .then(svgData => {
+      svgData = svgData.replace('<svg', '<svg fill="#ffff00"');
+      dropdownButton.innerHTML = `<h2 class='dropdown-menue'>${svgData}</h2><div class="options"></div`;
+      header.appendChild(dropdownButton);
+      const options = document.getElementsByClassName('options');
+      options[0].style.display = 'none';
+      //console.log(options);
+      const htmlFiles = ['memory.html', 'quizgame.html', 'megamario.html'];
+      htmlFiles.forEach(htmlFile => {
+        console.log('htmlfile ', htmlFile);
+        const option = document.createElement('option');
+        const linkNameArr = htmlFile.split('.');
+        option.innerHTML = `<a href=${htmlFile}>${linkNameArr[0]}</a>`;
+        options[0].appendChild(option);
+      })
 
-dropdownButton.addEventListener('click', () => {
-  if (options[0].style.display === 'block') {
-    options[0].style.display = 'none';
-  } else {
-    options[0].style.display = 'block';
-  }
-});
+    dropdownButton.addEventListener('click', () => {
+      if (options[0].style.display === 'block') {
+        options[0].style.display = 'none';
+      } else {
+        options[0].style.display = 'block';
+      }
+    });
+})
