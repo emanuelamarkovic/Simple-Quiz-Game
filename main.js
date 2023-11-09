764c34bce791d8deffd5a1947534b981528be5d9
panda <p4nd4@posteo.de>
2023-11-09 10:57:35 +0100

    work on dropdown-menue

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-import { questions } from './Questions.js';
 // --------- drop-down -------------
 const header = document.querySelector('header');
 const dropdownButton = document.createElement('div');
@@ -6,12 +5,14 @@ dropdownButton.classList = 'dropdown-menue';
 dropdownButton.innerHTML = `<h2 class='dropdown-menue'>X</h><div class="options"></div`;
 header.appendChild(dropdownButton);
 const options = document.getElementsByClassName('options');
+options[0].style.display = 'none';
 //console.log(options);
 const htmlFiles = ['memory.html', 'index.html'];
 htmlFiles.forEach(htmlFile => {
-  console.log('htmlfile ', htmlFile)
+  console.log('htmlfile ', htmlFile);
   const option = document.createElement('option');
-  option.innerHTML = `<a href=${htmlFile}>${htmlFile}</a>`;
+  const linkNameArr = htmlFile.split('.');
+  option.innerHTML = `<a href=${htmlFile}>${linkNameArr[0]}</a>`;
   options[0].appendChild(option);
 })
 
