ccada352936f866a5cf620e2797d916f16817b69
panda <p4nd4@posteo.de>
2023-11-11 00:22:32 +0100

    mega mario: add instructions

--- megamario.js
+++ megamario.js
@@ -1,34 +1,45 @@
+const main = document.querySelector('main')
+
 const mario = document.getElementById('mario');
 mario.style.position= 'relative';
-const imgMario = mario.getElementsByTagName('img');
+
+const imgMario = mario.querySelector('#mario>img');
 // initial x-axe value
-imgMario[0].style.transform = "translateX(0)";
+imgMario.style.transform = "translateX(0)";
 // matches numbers in style attributes
 const regex = /(?:[^0-9]+)?(\d+)(?:[^0-9]+)?/;
-let marioXPosition = parseInt(imgMario[0].style.transform.match(regex).slice(1));
+let marioXPosition = parseInt(imgMario.style.transform.match(regex).slice(1));
+
+instructions = document.createElement('div');
+instructions.style.textAlign = 'center';
+instructions.innerHTML = '<br><p>turn Mario with arrow keys left < or > right</p><br><p>make sure to enable autoplay media in your browser</p>';
+
 const marioTheme = new Audio('assets/516010__enviromaniac2__super-mario-bros-theme-techno-loop.mp3')
-marioTheme.addEventListener('ended', function() {
-  this.currentTime = 0;
-  this.play();
+// loop theme
+marioTheme.addEventListener('ended', () => {
+  marioTheme.currentTime = 0;
+  marioTheme.play();
 }, false);
-marioTheme.play()
+// play theme
+marioTheme.play();
+
 const stopMario = () => {
-    imgMario[0].src = "./assets/mario-stand.gif";
+    imgMario.src = "./assets/mario-stand.gif";
 }
 const moveMario = (event) => {
   //console.log('keydown!');
   //console.log(event.key);
-  if (!(imgMario[0].src = "./assets/mario-walk.gif")) {
-    imgMario[0].src = "./assets/mario-stand.gif";
+  if (imgMario.src !== "./assets/mario-walk.gif") {
+    imgMario.src = "./assets/mario-stand.gif";
   };
   if (event.key === 'ArrowLeft') {
     // move left
     marioXPosition -= 10;
-    imgMario[0].style.transform = `translateX(${marioXPosition}px) scaleX(-1)`;
+    imgMario.style.transform = `translateX(${marioXPosition}px) scaleX(-1)`;
   } else if (event.key === 'ArrowRight') {
     //console.log("right! ")
     marioXPosition += 10;
-    imgMario[0].style.transform = `translateX(${marioXPosition}px) scaleX(1)`;
+    imgMario.style.transform = `translateX(${marioXPosition}px) scaleX(1)`;
   }
 };
 document.addEventListener('keyup', stopMario);
