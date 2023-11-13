b379fcf90548dcbfcd4f1889f0b1b97fc0af5d05
panda <p4nd4@posteo.de>
2023-11-13 10:20:32 +0100

    add project description to startpage, add startpage to burger menue

--- main.js
+++ main.js
@@ -9,16 +9,22 @@ fetch('burger-icon.svg')
       svgData = svgData.replace('<svg', '<svg fill="#ffff00"');
       dropdownButton.innerHTML = `<h2 class='dropdown-menue'>${svgData}</h2><div class="options"></div`;
       header.appendChild(dropdownButton);
-      const options = document.getElementsByClassName('options');
-      options[0].style.display = 'none';
+      const options = document.querySelector('.options');
+      options.style.display = 'none';
       //console.log(options);
-      const htmlFiles = ['memory.html', 'quizgame.html', 'megamario.html'];
+      const htmlFiles = ['memory.html', 'quizgame.html', 'megamario.html', 'index.html'];
+
       htmlFiles.forEach(htmlFile => {
         console.log('htmlfile ', htmlFile);
         const option = document.createElement('option');
         const linkNameArr = htmlFile.split('.');
-        option.innerHTML = `<a href=${htmlFile}>${linkNameArr[0]}</a>`;
-        options[0].appendChild(option);
+        if (htmlFile === 'index.html') {
+          option.innerHTML = `<a href=./${htmlFile}>start</a>`;
+          options.appendChild(option);
+        } else {
+          option.innerHTML = `<a href=./${htmlFile}>${linkNameArr[0]}</a>`;
+          options.appendChild(option);
+        }
       })
 
     dropdownButton.addEventListener('click', () => {
